9aacfe1ecc81e11197ce4560efb30152a039568f
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-07-05 10:58:56 -0700

    uploaded project, No multipalyer feature atm

---
+++ Untitled/Source/Untitled/UntitledGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UntitledGameMode.h"
+#include "UntitledHUD.h"
+#include "UntitledCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AUntitledGameMode::AUntitledGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AUntitledHUD::StaticClass();
+}
